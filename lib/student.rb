278f292a7dbfb3470b2bfacc71698734c22f0be2
Travis Montgomery <travisbm@gmail.com>
2015-08-27 12:04:14 -0500

    added two files to /lib, added Student class to student.rb

---
+++ lib/student.rb
@@ -0,0 +1,17 @@
+class Student
+  attr_reader   :id
+  attr_accessor :gpa, :location
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name  = last_name
+    @id         = rand(1000) #ID automatically set to random number (1 - 1000)
+    @gpa        = 0
+  end
+
+  def self.at_location(location, first_name, last_name)
+    student = Student.new(first_name, last_name)
+    student.location = location
+    student
+  end
+end
